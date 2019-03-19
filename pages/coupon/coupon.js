a0df305451ba9145272bb9cb8e03b7a4ca5a042b
曾以祥 <n@example.com>
2019-03-19 11:27:02 +0800

    1

--- pages/coupon/coupon.js
+++ pages/coupon/coupon.js
@@ -15,6 +15,9 @@ Page({
     //用户积分
     integral: app.globalData.integral,
     listdata: [],
+    btnindex:"0",
+    listdata2:[],
+    listdata3:[],
   },
 
   /**
@@ -34,13 +37,14 @@ Page({
     if (app.globalData.getLocation == false) {
       this.getAddress()
     }
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.couponajax(0)
   },
 
   /**
@@ -83,5 +87,50 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  //获取优惠券数量
+  couponajax(status){
+    let that = this;
+    let time = app.time();
+    let data = {
+      'time': time
+    }
+    let str = app.signature(data, app.globalData.key);
+    wx.request({
+      url: app.globalData.url + '/Member/coupon_list', // 仅为示例，并非真实的接口地址
+      method: 'post',
+      data: {
+        'openid': app.globalData.openid,
+        'absign': str,
+        'time': time,
+        'member_id': app.globalData.member_id,
+        "status": status,
+      },
+      success(res) {
+        console.log(res.data);
+        if (status==0){
+          that.setData({
+            listdata: res.data,
+          })
+        } else if (status == 7){
+          that.setData({
+            listdata2: res.data,
+          })
+        }else{
+          that.setData({
+            listdata3: res.data,
+          })
+        }
+        
+      }
+    })
+  },
+  //点击切换优惠券
+  btn:function(e){
+    console.log(e.currentTarget.dataset.index);
+    this.setData({
+      btnindex: e.currentTarget.dataset.index
+    })
+    this.couponajax(e.currentTarget.dataset.index)
   }
 })
